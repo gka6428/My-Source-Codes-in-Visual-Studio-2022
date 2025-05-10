30ac3aa00801d9521fcf41cb104e286bd686c774
gka6428 <162865825+gka6428@users.noreply.github.com>
2025-05-10 18:17:42 +0900

    I complete to write down my source codes.

--- 자료구조10주차과제/자료구조10주차과제.cpp
+++ 자료구조10주차과제/자료구조10주차과제.cpp
@@ -2,7 +2,10 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <ctype.h>
+#define ERROR 0
 #define MAXLEN 1000
+#define FIRST_ID 2024100
+#define LAST_ID 2024199
 
 /*
 학번을 기준으로 학생 정보를 저장하는 간단한 검색 시스템을 만들고자 한다.
@@ -19,7 +22,7 @@
 */
 
 typedef unsigned int element;
-typedef char* String;
+typedef const char* String;
 typedef struct TreeNode {
 	element key;
 	String name;
@@ -27,66 +30,138 @@ typedef struct TreeNode {
 } TreeNode;
 
 TreeNode* search(TreeNode*, element);
-TreeNode* insert_node(TreeNode*, element);
-TreeNode* new_node(element);
+TreeNode* insert_node(TreeNode*, element, String);
+TreeNode* new_node(element, String);
 TreeNode* delete_node(TreeNode*, element);
 TreeNode* min_value_node(TreeNode*);
-int execute_interface(void);
-int getline(char[], int);
+int execute_interface(TreeNode *);
+int getID(char[], int);
+void print_nodes(TreeNode*);
 
 int main(void)
 {
-	while (execute_interface() != EOF)
+	TreeNode* root = NULL;
+	element ID = FIRST_ID;
+	for (; ID <= LAST_ID; ID++)
+		root = insert_node(root, ID, "김**"); /* 이름은 작동을 확인하기 위해 김**로 통일 */
+	while (execute_interface(root) != EOF)
 		;
 
 	return 0;
 }
 
 
-int execute_interface(void)
+int execute_interface(TreeNode* node)
 {
 	int ch;
 
-	printf("==== 학생 정보 시스템 ====\n");
+	printf("\n==== 학생 정보 시스템 ====\n");
 	printf("(1) 학생 정보 탐색\n");
 	printf("(2) 학생 정보 삭제\n");
 	printf("(3) 전체 출력\n");
 	printf("(0) 종료\n");
 	printf("선택: ");
-	if (isdigit(ch = getchar())) {
-		if (ch < 0 || ch > 3) {
-			printf("illegal input\n");
+	if (isdigit(ch = getchar())) {	/* check whether option(ch) was typed as a number */
+		if (ch < '0' || ch > '3') {		/* it is a number but not an expected option */
+			printf("\nerror: illegal input\n");
 			printf("usage: input 0, 1, 2, 3 only\n");
-			return -1;
+			getchar(); /* remove a remained character in input buffer from getchar */
+			return 0;
 		}
-		else {
+		else {						/* it is a number that represent an expected option */
 			element key;
 			char str[MAXLEN];
-			putchar('\n');
-			printf("학번 입력: ");
-			if (getline(str, MAXLEN) > 0){
+			TreeNode* temp;
 
+			putchar('\n');
+			switch (ch)
+			{
+			case '1':
+				printf("학번 입력: ");
+				if (getID(str, MAXLEN) > 0) {
+					for (int i = 0; str[i] != '\0'; i++)
+						if (!isdigit(str[i])) {
+							printf("error: illegal input about Student ID\n");
+							printf("usage: type your Student ID properly using numbers only\n");
+							return ERROR;
+						}
+
+					temp = search(node, atoi(str));
+					if (temp == NULL) {
+						printf("해당 학번 없음.\n");
+						break;
+					}
+					printf("검색 결과: 학번: %d, 이름: %s\n", temp->key, temp->name);
+					putchar('\n');
+					break;
+				}
+				else { /* the length of str is 0 */
+					printf("error: illegal input about Student ID\n");
+					printf("usage: type your Student ID properly\n");
+					return ERROR;
+				}
+			case '2':
+				printf("삭제할 학번 입력: ");
+				if (getID(str, MAXLEN) > 0) {
+					for (int i = 0; str[i] != '\0'; i++)
+						if (!isdigit(str[i])) {
+							printf("error: illegal input about Student ID\n");
+							printf("usage: type your Student ID properly using numbers only\n");
+							return ERROR;
+						}
+
+					temp = search(node, atoi(str));
+					if (temp == NULL) {
+						printf("해당 학번 없음.\n");
+						break;
+					}
+					printf("학번 %d 삭제 완료.\n", temp->key);
+					putchar('\n');
+					break;
+				}
+				else {
+					printf("error: illegal input about Student ID\n");
+					printf("usage: type your Student ID properly\n");
+					return ERROR;
+				}
+			case '3': 
+				printf("\n\n--- 전체 학생 목록 (오름차순) ---\n");
+				print_nodes(node);
+				getchar(); /* remove a remained character in input buffer from getchar */
+				break;
+			case '0': /* user wants to terminate this program */
+				return EOF;
 			}
-			
+			return 0;
 		}
 	}
-	else if (ch == EOF)
-		return ch;
-	else {
-		printf("illegal input: input is not a number\n");
+	else {				/* the option(ch) was typed unproperly */
+		printf("\nerror: input is not a number\n");
 		printf("usage: input 0, 1, 2, 3 only\n");
-		return -1;
+		getchar(); /* remove a remained character in input buffer from getchar */
+		return ERROR;
 	}
 }
 
-int getline(char line[], int lim)
+int getID(char line[], int lim)
 {
 	int c, i = 0;
+	
+	c = getchar(); /* remove a remained character in input buffer from getchar */
 	while (--lim > 0 && (c = getchar()) != '\n')
 		line[i++] = c;
 	line[i] = '\0';
 
 	return i;
+}
+
+void print_nodes(TreeNode* root)
+{
+	if (root) {
+		print_nodes(root->left);
+		printf("학번: %d, 이름: %s\n", root->key, root->name);
+		print_nodes(root->right);
+	}
 }
 
 TreeNode* search(TreeNode* node, element key)
@@ -97,28 +172,29 @@ TreeNode* search(TreeNode* node, element key)
     if (key == node->key)
         return node;
     else if (key < node->key)             
-        return  search(node->left, key);  /* search left subtree */
+        return search(node->left, key);  /* search left subtree */
     else
-        return  search(node->right, key); /* search right subtree */
+        return search(node->right, key); /* search right subtree */
 }
 
-TreeNode* insert_node(TreeNode* root, element key)
+TreeNode* insert_node(TreeNode* root, element key, String name)
 {
 	if (root == NULL)
-		return new_node(key);
+		return new_node(key, name);
 
 	if (key < root->key)
-		root->left = insert_node(root->left, key);
+		root->left = insert_node(root->left, key, name);
 	else if (key > root->key)
-		root->right = insert_node(root->right, key);
+		root->right = insert_node(root->right, key, name);
 
 	return root;
 }
 
-TreeNode* new_node(element item)
+TreeNode* new_node(element item, String name)
 {
 	TreeNode* temp = (TreeNode*)malloc(sizeof(TreeNode));
 	temp->key = item;
+	temp->name = name;
 	temp->left = temp->right = NULL;
 
 	return temp;
